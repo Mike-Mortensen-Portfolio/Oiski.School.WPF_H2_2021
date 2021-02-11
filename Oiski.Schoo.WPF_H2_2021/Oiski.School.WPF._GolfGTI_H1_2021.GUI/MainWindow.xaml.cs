2fb4c0812fe8280bb3f572d5bf6c87cc6a1a7a3e
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-02-11 12:58:24 +0100

    Finnished Assignment

---
+++ Oiski.Schoo.WPF_H2_2021/Oiski.School.WPF._GolfGTI_H1_2021.GUI/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oiski.Schoo.WPF_H1_2021.FrameworkGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow ()
+        {
+            InitializeComponent ();
+        }
+    }
+}
